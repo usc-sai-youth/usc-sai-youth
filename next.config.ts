bde19aeffd7f32d646e6756159d3754b305d823f
Vincent Chen <yinhsuac@uci.edu>
2026-07-15 10:46:11 -0700

    fix: modified basePath and assetPrefix in next.config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  }
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/usc-sai-youth' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/usc-sai-youth/' : ''
 };
 
 export default nextConfig;
