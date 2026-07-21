6fb465c83f8b7a9749ed0a5f48c9331965199929
Vincent Chen <yinhsuac@uci.edu>
2026-07-21 14:06:57 -0700

    rearrange: create hero component and adjust tab size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,44 +5,44 @@ import moeaLogo from "@/public/logos/moea-logo.png"
 import uscLawLogo from "@/public/logos/usc-law-logo.png"
 
 export default function Footer() {
-    return (
-        <div className="pt-5 pb-4 bg-black text-white">
-            <div className="flex flex-col md:flex-row justify-center items-center gap-4">
-                <div className="flex flex-row items-center">
-                    <p>主辦單位：</p>
-                    <Image
-                        src={moeaLogo}
-                        alt="Administration of Commerce, MOEA"
-                        width={120}
-                        height={40}
-                        className="h-[40px] w-auto"
-                    />
-                </div>
-                <p>執行單位：商業服務業 AI 人才辦公室</p>
-                <div className="flex flex-row items-center">
-                    <p>承辦單位：</p>
-                    <Image
-                        src={uscLawLogo}
-                        alt="Shih Chien Law"
-                        width={120}
-                        height={40}
-                        className="h-[40px] w-auto"
-                    />
-                </div>
-            </div>
-            <div className="mt-10 flex flex-col md:flex-row justify-center items-center">
-                <p className="text-center">計畫主持人：郝鳳鳴 教授</p>
-                <span className="hidden md:inline mx-2">|</span>
-                <p className="text-center">計畫聯絡窗口：林志丞 助理教授 (0979-838-333)</p>
-            </div>
-            <div className="mt-5 flex flex-col md:flex-row justify-center items-center">
-                <p className="text-center">聯絡信箱：cclin@g2.usc.edu.tw</p>
-                <span className="hidden md:inline mx-2">|</span>
-                <p className="text-center">地址：台北市中山區大直街 70 號</p>
-            </div>
-            <div className="mt-10">
-                <h5 className="text-center text-slate-600">© 2026 實踐大學. All Rights Reserved. 個人資料保護與隱私權聲明</h5>
-            </div>
+  return (
+    <div className="pt-5 pb-4 bg-black text-white">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+        <div className="flex flex-row items-center">
+          <p>主辦單位：</p>
+          <Image
+            src={moeaLogo}
+            alt="Administration of Commerce, MOEA"
+            width={120}
+            height={40}
+            className="h-[40px] w-auto"
+          />
         </div>
-    )
+        <p>執行單位：商業服務業 AI 人才辦公室</p>
+        <div className="flex flex-row items-center">
+          <p>承辦單位：</p>
+          <Image
+            src={uscLawLogo}
+            alt="Shih Chien Law"
+            width={120}
+            height={40}
+            className="h-[40px] w-auto"
+          />
+        </div>
+      </div>
+      <div className="mt-10 flex flex-col md:flex-row justify-center items-center">
+        <p className="text-center">計畫主持人：郝鳳鳴 教授</p>
+        <span className="hidden md:inline mx-2">|</span>
+        <p className="text-center">計畫聯絡窗口：林志丞 助理教授 (0979-838-333)</p>
+      </div>
+      <div className="mt-5 flex flex-col md:flex-row justify-center items-center">
+        <p className="text-center">聯絡信箱：cclin@g2.usc.edu.tw</p>
+        <span className="hidden md:inline mx-2">|</span>
+        <p className="text-center">地址：台北市中山區大直街 70 號</p>
+      </div>
+      <div className="mt-10">
+        <h5 className="text-center text-slate-600">© 2026 實踐大學. All Rights Reserved. 個人資料保護與隱私權聲明</h5>
+      </div>
+    </div>
+  )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,68 +1,68 @@
 'use client';
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 import uscLogo from "@/public/logos/usc-logo.png"
 import moeaLogo from "@/public/logos/moea-logo.png"
-export default function Navbar (){
-    const [menuOpen, setMenuOpen] = useState(false);
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen((prev) => !prev)
-    }
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev)
+  }
 
-    const closeMenu = () => {
-        setMenuOpen(false)
-    }
+  const closeMenu = () => {
+    setMenuOpen(false)
+  }
 
-    return (
-        <div className="top-3 inset-x-0 fixed flex flex-col gap-2 transform-gpu will-change-transform">
-            <div className="mx-5 md:mx-10 px-5 py-2 md:py-4 box-border flex justify-between items-center border border-white/30 shadow-lg shadow-black/10 rounded-full bg-white/20 backdrop-blur-md">
-                <div className="flex gap-2">
-                    <Image
-                        src={uscLogo}
-                        alt="Shih Chien University"
-                        width={90}
-                        height={30}
-                        className="h-[20px] md:h-[30px] w-auto"
-                    />
-                    <Image
-                        src={moeaLogo}
-                        alt="Administration of Commerce, MOEA"
-                        width={90}
-                        height={30}
-                        className="hidden sm:inline h-[20px] md:h-[30px] w-auto"
-                    />
-                </div>
-                <div className="hidden lg:inline lg:flex lg:items-center lg:gap-2 lg:text-[18px]">
-                    <a href="#core">課程核心</a>
-                    <a href="#classes">實戰班別</a>
-                    <a href="#highlights">計畫亮點</a>
-                    <a href="#process">甄選流程</a>
-                    <button className="apply-btn ml-2">我要報名</button>
-                </div>
-                {menuOpen ? 
-                    <XMarkIcon className="lg:hidden w-6 h-6 text-black hover:text-gray-500 cursor-pointer" onClick={toggleMenu}/> :
-                    <Bars3Icon className="lg:hidden w-6 h-6 text-black hover:text-gray-500 cursor-pointer" onClick={toggleMenu}/>
-                }
-            </div>
-            {menuOpen && (
-                <div className="glass lg:hidden mx-5 md:mx-10 p-6 flex flex-col gap-2 items-center rounded-3xl bg-slate-400/100 text-black backdrop-blur-md">
-                    <a href="#core" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">課程核心</a>
-                    <a href="#classes" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">實戰班別</a>
-                    <a href="#highlights" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">計畫亮點</a>
-                    <a href="#process" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">甄選流程</a>
-                    <button 
-                        onClick={closeMenu}
-                        className="apply-btn w-full h-10"
-                    >
-                        我要報名
-                    </button>
-                </div>
-            )}
+  return (
+    <div className="top-3 inset-x-0 fixed flex flex-col gap-2 transform-gpu will-change-transform z-50">
+      <div className="mx-5 md:mx-10 px-5 py-2 md:py-4 box-border flex justify-between items-center border border-white/30 shadow-lg shadow-black/10 rounded-full bg-white/20 backdrop-blur-md">
+        <div className="flex gap-2">
+          <Image
+            src={uscLogo}
+            alt="Shih Chien University"
+            width={90}
+            height={30}
+            className="h-[20px] md:h-[30px] w-auto"
+          />
+          <Image
+            src={moeaLogo}
+            alt="Administration of Commerce, MOEA"
+            width={90}
+            height={30}
+            className="hidden sm:inline h-[20px] md:h-[30px] w-auto"
+          />
         </div>
-    );
+        <div className="hidden lg:inline lg:flex lg:items-center lg:gap-2 lg:text-[18px]">
+          <a href="#core">課程核心</a>
+          <a href="#classes">實戰班別</a>
+          <a href="#highlights">計畫亮點</a>
+          <a href="#process">甄選流程</a>
+          <button className="apply-btn ml-2">我要報名</button>
+        </div>
+        {menuOpen ?
+          <XMarkIcon className="lg:hidden w-6 h-6 text-black hover:text-gray-500 cursor-pointer" onClick={toggleMenu} /> :
+          <Bars3Icon className="lg:hidden w-6 h-6 text-black hover:text-gray-500 cursor-pointer" onClick={toggleMenu} />
+        }
+      </div>
+      {menuOpen && (
+        <div className="glass lg:hidden mx-5 md:mx-10 p-6 flex flex-col gap-2 items-center rounded-3xl bg-slate-400/100 text-black backdrop-blur-md">
+          <a href="#core" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">課程核心</a>
+          <a href="#classes" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">實戰班別</a>
+          <a href="#highlights" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">計畫亮點</a>
+          <a href="#process" onClick={closeMenu} className="transition-colors w-full h-10 flex items-center justify-center">甄選流程</a>
+          <button
+            onClick={closeMenu}
+            className="apply-btn w-full h-10"
+          >
+            我要報名
+          </button>
+        </div>
+      )}
+    </div>
+  );
 } 
