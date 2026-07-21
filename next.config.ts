dba55ebca09b296337485ca1d1f9d8cef9419eea
Vincent Chen <yinhsuac@uci.edu>
2026-07-20 17:12:31 -0700

    fix: update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/usc-sai-youth' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/usc-sai-youth/' : ''
+  basePath: isGithubActions ? '/usc-sai-youth' : '',
+  assetPrefix: isGithubActions ? '/usc-sai-youth/' : '',
 };
 
 export default nextConfig;
