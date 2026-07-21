53a4d26e9e2ac6a38c65a4395555f4ac8f14b5cb
Vincent Chen <yinhsuac@uci.edu>
2026-07-21 15:42:25 -0700

    feat: general course structure section implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Core from "../components/Core";
+import GeneralStructure from "../components/GeneralStructure";
 import Hero from "../components/Hero";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Hero/>
       <Core/>
+      <GeneralStructure/>
     </>
   );
 }
