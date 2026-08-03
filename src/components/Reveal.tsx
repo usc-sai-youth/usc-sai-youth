838f656e8b9d303de375c468116a162679895509
Vincent Chen <yinhsuac@uci.edu>
2026-08-02 18:43:12 -0700

    fix: fixed section not showing err

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -30,7 +30,7 @@ export default function Reveal({
           observer.unobserve(el);
         }
       },
-      { threshold: 0.15, rootMargin: "0px 0px -10% 0px" }
+      { threshold: 0, rootMargin: "0px 0px -10% 0px" }
     );
 
     observer.observe(el);
