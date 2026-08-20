3798d41482c25c2fa0fbb8b86563927fd488e1d5
Vincent Chen <yinhsuac@uci.edu>
2026-08-19 20:56:14 -0700

    info: update contact information and logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 import moeaLogo from "@/public/logos/moea-white-logo.png"
-import uscLawLogo from "@/public/logos/usc-law-white-logo.png"
+import uscLawLogo from "@/public/logos/usc-law-school-white.png"
 
 export default function Footer() {
   return (
@@ -62,10 +62,10 @@ export default function Footer() {
         <div className="mt-10 flex flex-col md:flex-row justify-center items-center">
           <p className="text-center">計畫主持人：郝鳳鳴 教授</p>
           <span className="hidden md:inline mx-2">|</span>
-          <p className="text-center">計畫聯絡窗口：陳毓雯 老師 (02)25381111 轉 6118</p>
+          <p className="text-center">計畫聯絡窗口：袁廣樺 老師 0982-506-411</p>
         </div>
         <div className="mt-5 flex flex-col md:flex-row justify-center items-center">
-          <p className="text-center">聯絡信箱：sheba@g2.usc.edu.tw</p>
+          <p className="text-center">聯絡信箱：bio0313@ghotmail.com</p>
           <span className="hidden md:inline mx-2">|</span>
           <p className="text-center">地址：台北市中山區大直街 70 號</p>
         </div>
